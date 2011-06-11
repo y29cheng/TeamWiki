0ab04df3da0d78b43c69bf83bdf47ba4f190186b
fog <fog@apache01.(none)>
2011-06-11 22:07:25 +0000

    Configuring basic setup

--- app/config/core.php
+++ app/config/core.php
@@ -200,12 +200,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'b6ce1a80f9f8c660953c525262');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '0.474296931051687');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
