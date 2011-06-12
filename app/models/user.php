ed7505c5d894f53754acba10c9668196bf2cfdc6
y29cheng <y29cheng@gmail.com>
2011-06-12 03:15:14 -0400

    added user registration

--- app/models/user.php
+++ app/models/user.php
@@ -2,9 +2,9 @@
 class User extends AppModel {
 	public $name = 'User';
 	public $validate = array (
-		'username' => VALID_NOT_EMPTY,
-		'password' => VALID_NOT_EMPTY,
-		'email' =>VALID_EMAIL
+		'username' => 'notEmpty',
+		'password' => 'notEmpty',
+		'email' => 'notEmpty'
 	);
 }
 ?>
