e87261fda31f13ce0f26b7a226c0ca4f04ed1670
y29cheng <y29cheng@gmail.com>
2011-06-11 20:14:24 -0400

    configed routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -26,10 +26,10 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/views/pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 	
-//	Router::connect('/', array('controller' => 'logins', 'action' => 'index'));
+	Router::connect('/', array('controller' => 'posts', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+
