78a56003931ce7fef272540d1c4f73ca21f3ffb1
George Cheng <y29cheng@gmail.com>
2011-11-08 10:55:54 -0500

    small fix

--- app/webroot/js/clock.js
+++ app/webroot/js/clock.js
@@ -2,5 +2,5 @@ function clock() {
 	setTimeout("clock()", 1000);
 	now = new Date();
 	current_time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-	return current_time;
+	document.write current_time;
 }
