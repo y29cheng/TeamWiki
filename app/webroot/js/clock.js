f19a44a9c35d2dd8797d0cef2ea11f850891e35c
George Cheng <y29cheng@gmail.com>
2011-11-08 13:50:01 -0500

    small fixes

--- app/webroot/js/clock.js
+++ app/webroot/js/clock.js
@@ -1,5 +1,11 @@
 function clock() {
 	var now = new Date();
-	var timeString = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+	var hours = now.getHours();
+	var minutes = now.getMinutes();
+	var seconds = now.getSeconds();
+	if (hours < 10) hours = "0" + hours;
+	if (minutes < 10) minutes = "0" + minutes;
+	if (seconds < 10) seconds = "0" + seconds;
+	var timeString = hours + ":" + minutes + ":" + seconds;
 	document.getElementById("clock").firstChild.nodeValue = timeString;
 }
