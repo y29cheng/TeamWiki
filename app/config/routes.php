22e10e7632b3af9042ec7efeb04987717a836484
George Cheng <y29cheng@gmail.com>
2011-11-11 15:00:34 -0500

    small change

--- app/config/routes.php
+++ app/config/routes.php
@@ -28,7 +28,7 @@
  */
 	//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 	
-//	Router::connect('/', array('controller' => 'users', 'action' => 'register'));
+	Router::connect('/', array('controller' => 'users', 'action' => 'register'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
