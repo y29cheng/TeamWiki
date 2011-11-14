dbb16f3ab3b60825f1db7842b742a5d5f6ceb326
George Cheng <y29cheng@gmail.com>
2011-11-14 16:59:17 -0500

    small fix

--- app/webroot/register.php
+++ app/webroot/register.php
@@ -3,12 +3,13 @@ require('db_info.php');
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$encrypt = md5($password);
 $connect = mysql_connect($host, $user, $pass) or die ("unable to connect to".$host);
 mysql_select_db($db, $connect);
 $query = "select * from users where username='$username'";
 $result = mysql_query($query);
 if (!mysql_num_rows($result)) {
-	$query = "insert into users(username, password, email) values($username, $password, $email)";
+	$query = "insert into users(username, password, email) values($username, $encrypt, $email)";
 	$result = mysql_query($query);
 	if (!$result) echo 2;
 	else echo 0;
