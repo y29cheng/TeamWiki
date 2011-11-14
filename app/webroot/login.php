b931478993dba6c227e340acee4809ab14d22b6c
George Cheng <y29cheng@gmail.com>
2011-11-14 15:09:54 -0500

    added login.php

---
+++ app/webroot/login.php
@@ -0,0 +1,15 @@
+<?php
+require('db_info.php');
+$username = $_POST['username'];
+$password = $_POST['password'];
+$encrypt = md5($password);
+$connect = mysql_connect($host, $user, $pass) or die ("unable to connect to host");
+mysql_select_db($db, $connect);
+$query = "select * from users where username='$username' and password='$encrypt'";
+$result = mysql_query($query);
+if (!mysql_num_rows($result)) {
+        echo 0;
+} else {
+        echo 1;
+}
+?>
