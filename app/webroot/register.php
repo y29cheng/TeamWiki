e19d3d05acb05fd5beecfe04c4b38679d42671df
George Cheng <y29cheng@gmail.com>
2011-11-14 15:58:35 -0500

    small fix

--- app/webroot/register.php
+++ app/webroot/register.php
@@ -3,7 +3,7 @@ require('db_info.php');
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
-$connect = mysql_connect($host, $user, $pass) or die ("unable to connect to host");
+$connect = mysql_connect($host, $user, $pass) or die ("unable to connect to".$host);
 mysql_select_db($db, $connect);
 $query = "select * from users where username='$username'";
 $result = mysql_query($query);
