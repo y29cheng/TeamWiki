ded7feaf64e8b56d76ef9d1688d17aa220b44ecf
George Cheng <y29cheng@gmail.com>
2011-11-15 10:22:47 -0500

    small fix

--- app/webroot/json.php
+++ app/webroot/json.php
@@ -2,7 +2,7 @@
 require('db_info.php');
 $connect = mysql_connect($host, $user, $pass) or die ("unable to connect to ".$host);
 mysql_select_db($db, $connect);
-$query = "select id, title from votes";
+$query = "select id, title, choice1, choice2, choice3, choice4 from votes";
 $result = mysql_query($query);
 $arr = array();
 while ($obj = mysql_fetch_object($result)) {
