48256d322b4e4e36abd6df88578af377d60d43f9
George Cheng <y29cheng@gmail.com>
2011-11-18 16:32:15 -0500

    small fix

--- app/webroot/delete.php
+++ app/webroot/delete.php
@@ -6,6 +6,10 @@ $connect = mysql_connect($host, $user, $pass) or die ("unable to connect to ".$h
 mysql_select_db($db, $connect);
 $query = "delete from votes where id='$id'";
 $result = mysql_query($query);
+$redis = new iRedis(array('hostname' => '50.30.35.9', 'port' => 2117));
+$redis->auth('f0493aeaecd8799a1ecdb5ca9193e0e6');
+$redis->del('vote'.$id);
+$redis->del('voters'.$id);
 if ($result) {
 	echo 1;
 } else {
