6a193eade852f0403bbefa603ffde0ac59acfa97
George Cheng <y29cheng@gmail.com>
2011-11-19 00:27:55 -0500

    small fix

--- app/webroot/add.php
+++ app/webroot/add.php
@@ -11,6 +11,14 @@ $connect = mysql_connect($host, $user, $pass) or die ("unable to connect to ".$h
 mysql_select_db($db, $connect);
 $query = "insert into votes (title, owner, choice1, choice2, choice3, choice4) values ('$title', '$owner', '$choice1', '$choice2', '$choice3', '$choice4')";
 $result = mysql_query($query);
+$redis = new iRedis(array('hostname' => '50.30.35.9', 'port' => 2117));
+$redis->auth('f0493aeaecd8799a1ecdb5ca9193e0e6');
+$redis->incr('id');
+$tmp = $redis->get('id');
+$redis->hset('vote'.$tmp, 'a1', 0);
+$redis->hset('vote'.$tmp, 'a2', 0);
+$redis->hset('vote'.$tmp, 'a3', 0);
+$redis->hset('vote'.$tmp, 'a4', 0);
 if ($result) {
 	echo 1;
 } else {
