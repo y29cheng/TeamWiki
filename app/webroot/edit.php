73a0c4824b2f8b3d13b33704aa5325c3fef11900
George Cheng <y29cheng@gmail.com>
2011-11-19 01:01:08 -0500

    small fix

--- app/webroot/edit.php
+++ app/webroot/edit.php
@@ -1,5 +1,6 @@
 <?php
 require('db_info.php');
+$owner = $_POST['owner'];
 $title = $_POST['title'];
 $choice1 = $_POST['choice1'];
 $choice2 = $_POST['choice2'];
@@ -8,7 +9,7 @@ $choice4 = $_POST['choice4'];
 $modified = date("Y-m-d", time());
 $connect = mysql_connect($host, $user, $pass) or die ("unable to connect to ".$host);
 mysql_select_db($db, $connect);
-$query = "update votes set title='$title', choice1='$choice1', choice2='$choice2', choice3='$choice3', choice4='$choice4', modified='$modified'";
+$query = "update votes set title='$title', choice1='$choice1', choice2='$choice2', choice3='$choice3', choice4='$choice4', modified='$modified' where owner='$owner'";
 $result = mysql_query($query);
 if ($result) {
 	echo 1;
