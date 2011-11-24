a3258e7a18d50b34aa27b8dbb2bf56450ebcf590
George Cheng <y29cheng@gmail.com>
2011-11-24 11:45:53 -0500

    small fix

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -103,7 +103,7 @@ class UsersController extends AppController {
 		}
 		if (!empty($this->data['User']['password'])) {
 			$requester = $this->User->findByPassword(md5($this->data['User']['password']));
-			if ($requester && $requester === $this->Session->read('user')) {
+			if ($requester && $requester['User']['username'] == $this->Session->read('user')) {
 				if (!empty($this->data['User']['passwd']) && $this->data['User']['passwd'] === $this->data['User']['psword']) {
 					$update = update_password($requester['User']['password'], md5($this->data['User']['passwd']));
 					if ($update) {
@@ -115,7 +115,7 @@ class UsersController extends AppController {
 					$this->Session->setFlash('Passwords don\'t match.');
 				}
 			} else {
-				$this->Session->setFlash('Your password is wrong.'.' '.$requester);
+				$this->Session->setFlash('Your password is wrong.');
 			}
 		} else {
 			$this->Session->setFlash('Please provide your password.');
