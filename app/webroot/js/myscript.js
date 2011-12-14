f1f0f7e523214c359d1aa88b1121e969fc7a326f
George Cheng <y29cheng@gmail.com>
2011-12-13 21:54:49 -0500

    small fix

--- app/webroot/js/myscript.js
+++ app/webroot/js/myscript.js
@@ -3,7 +3,7 @@ function addInput(divName) {
 	var newDiv = document.createElement('div');
 	var counter = parentDiv.getElementsByTagName('div').length - 1;
 	newDiv.setAttribute('id', 'VoteChoice' + (counter + 1));
-	newDiv.setAttribute('class', 'input textarea required');
+	newDiv.setAttribute('class', 'input textarea');
 	newDiv.innerHTML = "<label for='VoteChoice" + (counter + 1) + "'>Choice" + (counter + 1) + "</label><textarea name='data[Vote][choice" + (counter + 1) + "]' cols='30' rows='6' id='VoteChoice" + (counter + 1) + "' value='Enter choice" + (counter + 1) + " here'></textarea>"
 	parentDiv.appendChild(newDiv);
 }
@@ -14,6 +14,6 @@ function deleteInput(divName) {
 		alert("You can have a least 3 rows.");
 	} else {
 		var badDiv = document.getElementById('VoteChoice' + counter);
-		dynamicDiv.removeChild(badDiv);
+		parentDiv.removeChild(badDiv);
 	}
 }
