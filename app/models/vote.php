327b87689d865abaca7098f7fed57ae760178bce
George Cheng <y29cheng@gmail.com>
2011-12-13 21:45:36 -0500

    small fix

--- app/models/vote.php
+++ app/models/vote.php
@@ -1,22 +1,4 @@
 <?php
 class Vote extends AppModel {
 	public $name = 'Vote';
-	public $validate = array(
-		'title' => array(
-			'rule' => 'notEmpty'
-		),
-		'choice1' => array(
-			'rule' => 'notEmpty'
-		),
-		'choice2' => array(
-                        'rule' => 'notEmpty'
-                ),
-		'choice3' => array(
-                        'rule' => 'notEmpty'
-                ),
-		'choice4' => array(
-                        'rule' => 'notEmpty'
-                )
-	);
-}
 ?>
