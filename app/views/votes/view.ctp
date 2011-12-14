6414fe86ce09124ee60ccc8fd433d7f86d330fd4
George Cheng <y29cheng@gmail.com>
2011-12-14 16:30:26 -0500

    added html5 bar graph

--- app/views/votes/view.ctp
+++ app/views/votes/view.ctp
@@ -6,10 +6,27 @@ for ($i=0;$i<$count;$i++) {
 ?>
 <p>
 <?php echo $vote['answer'.($i+1)]; ?>&nbsp&nbsp&nbsp&nbsp
-<?php echo $this->Html->link($vote['choice'.($i+1)], array('controller' => 'votes', 'action' => 'vote', $vote['_id'], $i+1)); ?>
+<?php echo $this->Html->link($vote['choice'.($i+1)], array('value' => $vote['answer'.($i+1)], 'controller' => 'votes', 'action' => 'vote', $vote['_id'], $i+1)); ?>
 </p>
 <?php } ?>
-
+<canvas id="bar_graph"></canvas>
+<?php echo $javascript->link('barGraph.js', false); ?>
+<script>
+var ctx = document.getElementById('bar_graph').getContext('2d');
+var graph = new BarGraph(ctx);
+var div = document.getElementById('content');
+var links = div.getElementsByTagName('a');
+var nbr = links.length;
+var arr = new Array();
+graph.margin = 2;
+graph.width = 450;
+graph.height = 150;
+for (var i = 0; i < nbr; i++) {
+	graph.xAxisLabelArr[i] = "Choice" + (i + 1);
+	arr[i] = links[i].getAttribute('value');
+}
+graph.update(arr);
+</script>
 <!--<form method="post" action="http://teamwiki.phpfogapp.com/bar_chart.php">
 <input type="hidden" name="id" value=<?php echo $id ?>>
 <input type="submit" value="Show Result">-->
---
+++ app/webroot/js/barGraph.js
@@ -0,0 +1,81 @@
+function BarGraph(ctx) {
+	//private
+	var that = this;
+	var.draw = function (arr) {
+		var numOfBars = arr.length;
+		var barWidth;
+		var barHeight;
+		var maxBarHeight;
+		var gradient;
+		var graphAreaWidth = that.width;
+		var graphAreaHeight = that.height;
+		
+		if (ctx.canvas.width !== that.width || ctx.canvas.height !== that.height) {
+			ctx.canvas.width = that.width;
+			ctx.canvas.height = that.height;
+		}
+		
+		ctx.fillStyle = that.bgColor;
+		ctx.fillRect(0, 0, that.width, that.height);
+		
+		if (that.xAxisLabelArr.length) {
+			graphAreaHeight -= 40;
+		} else {
+			return;
+		}
+		
+		barWidth = graphAreaWidth / numOfBars - that.margin * 2;
+		maxBarHeight = graphAreaHeight - 25;
+		
+		var maxValue = 0;
+		for (var i = 0; i < numOfBars; i++) {
+			if (arr[i] > maxValue) {
+				maxValue = arr[i];
+			}
+		}
+		
+		var ratio;
+		for (var i = 0; i < numOfBars; i++) {
+			ratio = arr[i] / maxValue;
+			barHeight = maxBarHeight * ratio;
+			
+			ctx.shadowOffsetX = 2;
+			ctx.shadowOffsetY = 2;
+			ctx.shadowBlur = 0;
+			ctx.shadowColor = "#999";
+			
+			ctx.fillStyle = "#333";
+			ctx.fillRect(that.width / numOfBars * i + that.margin, graphAreaHeight - barHeight, barWidth, barHeight);
+			
+			ctx.shadowOffsetX = 0;
+			ctx.shadowOffsetY = 0;
+			ctx.shadowBlur = 0;
+			
+			ctx.fillStyle "#333";
+			ctx.font = "bold 12px sans-serif";
+			ctx.textAligh = "center";
+			try {
+				ctx.fillText(parseInt(arr[i],10), i * that.width / numOfBars + (that.width / numOfBars) / 2, graphAreaHeight - barHeight - 10);
+			} catch (ex) {}
+			if (that.xAxisLabelArr[i]) {
+				ctx.fillStyle = "#333";
+				ctx.font = "bold 12px sans-serif";
+				ctx.textAlign = "center";
+				try {
+					ctx.fillText(that.xAxisLabelArr[i], i * that.width / numOfBars + (that.width / numOfBars) / 2, that.height - 10);
+			    } catch (ex) {}
+			}
+		}
+	};
+	//public
+	this.width = 450;
+	this.height = 150;
+	this.margin = 5;
+	this.xAxisLabelArr = [];
+	this.yAxisLabelArr = [];
+	this.bgColor = #fff;
+	
+	this.update = function(arr) {
+		draw(arr);
+	};
+}
