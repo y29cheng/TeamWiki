d8e14796900895f68d299030ce241817c9605298
George Cheng <y29cheng@gmail.com>
2011-12-14 00:42:42 -0500

    debugging

--- app/controllers/votes_controller.php
+++ app/controllers/votes_controller.php
@@ -93,8 +93,8 @@ class VotesController extends AppController {
 			$doc = $collection->findOne(array('_id' => new MongoId($id)));
 		} else {
 			$doc = $collection->findOne(array('_id' => new MongoId($this->data['Vote']['id'])));
-			$this->Session->setFlash($this->data['Vote']['id']."");
-			return;
+// 			$this->Session->setFlash($this->data['Vote']['id']."");
+// 			return;
 		}
 		$this->set('vote', $doc);
         $username = $this->Session->read('user');
