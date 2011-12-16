518b4c1b85b8f4149f46b86e896e0144494f7685
George Cheng <y29cheng@gmail.com>
2011-12-16 12:29:25 -0500

    recovered json

---
+++ app/webroot/json.php
@@ -0,0 +1,12 @@
+<?php
+require('db_info.php');
+$connect = mysql_connect($host, $user, $pass) or die ("unable to connect to ".$host);
+mysql_select_db($db, $connect);
+$query = "select id, owner, title, choice1, choice2, choice3, choice4 from votes";
+$result = mysql_query($query);
+$arr = array();
+while ($obj = mysql_fetch_object($result)) {
+        $arr[] = $obj;
+}
+echo '{"votes":'.json_encode($votes).'}';
+?>
