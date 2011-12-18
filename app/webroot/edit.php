6d671bd4be0e1faa9979f626a3e3db29ea1723df
George Cheng <y29cheng@gmail.com>
2011-12-18 18:31:03 -0500

    fixed an edit bug

--- app/webroot/edit.php
+++ app/webroot/edit.php
@@ -1,6 +1,7 @@
 <?php
 require('db_info.php');
 $connect = mysql_connect($host, $user, $pass) or die ("unable to connect to ".$host);
+$id = mysql_real_escape_string($_POST['id'], $connect);
 $owner = mysql_real_escape_string($_POST['owner'], $connect);
 $title = mysql_real_escape_string($_POST['title'], $connect);
 $choice1 = mysql_real_escape_string($_POST['choice1'], $connect);
@@ -9,7 +10,7 @@ $choice3 = mysql_real_escape_string($_POST['choice3'], $connect);
 $choice4 = mysql_real_escape_string($_POST['choice4'], $connect);
 $modified = date("Y-m-d", time());
 mysql_select_db($db, $connect);
-$query = "update votes set title='$title', choice1='$choice1', choice2='$choice2', choice3='$choice3', choice4='$choice4', modified='$modified' where owner='$owner'";
+$query = "update votes set title='$title', choice1='$choice1', choice2='$choice2', choice3='$choice3', choice4='$choice4', modified='$modified' where id='$id'";
 $result = mysql_query($query);
 if ($result) {
 	echo 1;
