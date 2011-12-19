9311b0aef06d0bb3e3d2da1c5fe6be509bcbf220
George Cheng <y29cheng@gmail.com>
2011-12-19 17:57:04 -0500

    read json from mongolab

--- app/webroot/json.php
+++ app/webroot/json.php
@@ -1,14 +1,30 @@
 <?php
-require('../db_info.php');
-$connect = mysql_connect($host, $user, $pass) or die ("unable to connect to ".$host);
-mysql_select_db($db, $connect);
-$query = "select id, owner, title, choice1, choice2, choice3, choice4 from votes";
-$result = mysql_query($query);
-$arr = array();
-while ($obj = mysql_fetch_object($result)) {
-        $arr[] = $obj;
+// require('../db_info.php');
+// $connect = mysql_connect($host, $user, $pass) or die ("unable to connect to ".$host);
+// mysql_select_db($db, $connect);
+// $query = "select id, owner, title, choice1, choice2, choice3, choice4 from votes";
+// $result = mysql_query($query);
+// $arr = array();
+// while ($obj = mysql_fetch_object($result)) {
+//         $arr[] = $obj;
+// }
+// echo '{"votes":'.json_encode($arr).'}';
+$url='https://api.mongolab.com/api/1/databases/teamwiki/collections/votes?apiKey=4eef72abe4b092a677cdf360'; //rss link for the twitter timeline
+print_r(get_data($url)); //dumps the content, you can manipulate as you wish to
+
+/* gets the data from a URL */
+
+function get_data($url)
+{
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch,CURLOPT_URL,$url);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
 }
-echo '{"votes":'.json_encode($arr).'}';
 // require('mongo_info.php');
 // $mongo = new Mongo("mongodb://".$mongo_user.":".$mongo_pass."@dbh54.mongolab.com:27547/teamwiki");
 // $mongodb = $mongo->teamwiki;
