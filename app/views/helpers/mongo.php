8d7cf21221e9322b6dad370c9a43fb0a0a1e8e59
George Cheng <y29cheng@gmail.com>
2011-12-19 11:34:25 -0500

    switch to mongolabfrom mongohq

--- app/views/helpers/mongo.php
+++ app/views/helpers/mongo.php
@@ -1,7 +1,7 @@
 <?php
 class MongoHelper extends AppHelper {
 	function connect($username, $password) {
-		$mongo = new Mongo("mongodb://".$username.":".$password."@staff.mongohq.com:10056/teamwiki");
+		$mongo = new Mongo("mongodb://".$username.":".$password."@dbh54.mongolab.com:27547/teamwiki");
 		$mongodb = $mongo->teamwiki;
 		$collection = $mongodb->votes;
 		return $collection;
