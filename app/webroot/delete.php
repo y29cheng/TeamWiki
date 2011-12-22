046e1d7e18b3b661b0cb9ea2903d38ce1c4f1941
George Cheng <y29cheng@gmail.com>
2011-12-22 15:52:18 -0500

    new delete.php for android

--- app/webroot/delete.php
+++ app/webroot/delete.php
@@ -1,18 +1,28 @@
 <?php
 require('../db_info.php');
-require('../vendors/iredis.php');
+// require('../vendors/iredis.php');
+// $id = $_POST['id'];
+// $connect = mysql_connect($host, $user, $pass) or die ("unable to connect to ".$host);
+// mysql_select_db($db, $connect);
+// $query = "delete from votes where id='$id'";
+// $result = mysql_query($query);
+// $redis = new iRedis(array('hostname' => '50.30.35.9', 'port' => 2117));
+// $redis->auth('f0493aeaecd8799a1ecdb5ca9193e0e6');
+// $redis->del('vote'.$id);
+// $redis->del('voters'.$id);
+// if ($result) {
+// 	echo 1;
+// } else {
+// 	echo 0;
+// }
 $id = $_POST['id'];
-$connect = mysql_connect($host, $user, $pass) or die ("unable to connect to ".$host);
-mysql_select_db($db, $connect);
-$query = "delete from votes where id='$id'";
-$result = mysql_query($query);
-$redis = new iRedis(array('hostname' => '50.30.35.9', 'port' => 2117));
-$redis->auth('f0493aeaecd8799a1ecdb5ca9193e0e6');
-$redis->del('vote'.$id);
-$redis->del('voters'.$id);
-if ($result) {
-	echo 1;
-} else {
-	echo 0;
+$m = new Mongo("mongodb://".$mongo_user.":".$mongo_pass."@dbh54.mongolab.com:27547/teamwiki");
+$db = $m->teamwiki;
+$votes = $db->votes;
+try {
+	$votes->remove(array('_id' => new MongoId($id)), array('safe' => true));
+	echo "success";
+} catch (Exception $e) {
+	echo "error";
 }
 ?>
