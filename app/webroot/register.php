efbfde31a89f6b60b50ac3456130b8f520d80322
George Cheng <y29cheng@gmail.com>
2011-12-23 00:07:08 -0500

    fixed account verification on peervote

--- app/webroot/register.php
+++ app/webroot/register.php
@@ -6,7 +6,7 @@ $email = mysql_real_escape_string($_POST['email'], $connect);
 $password = $_POST['password'];
 $encrypt = md5($password);
 mysql_select_db($db, $connect);
-$query = "select * from users where username='$username'";
+$query = "select * from users where username='$username' or email='$email'";
 $result = mysql_query($query);
 if (!mysql_num_rows($result)) {
 	$query = "insert into users(username, password, email) values('$username', '$encrypt', '$email')";
